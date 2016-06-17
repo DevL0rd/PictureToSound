4a08fbed4688951cefd1f019a9245360481fcbf3
dmhzmxn <dmhzmxn@users.noreply.github.com>
2016-06-17 22:56:08 +0000

    Add files via upload

---
+++ PictureToSound/Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SpeedLabel = New System.Windows.Forms.Label()
+        Me.Speed_Modifier = New System.Windows.Forms.TrackBar()
+        Me.FreqModLabel = New System.Windows.Forms.Label()
+        Me.Frequency_Modifier = New System.Windows.Forms.TrackBar()
+        Me.PlayButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Output = New System.Windows.Forms.TextBox()
+        Me.MenuView = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundSync = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextColorSync = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.Speed_Modifier, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Frequency_Modifier, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1024, 483)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.SpeedLabel)
+        Me.Panel1.Controls.Add(Me.Speed_Modifier)
+        Me.Panel1.Controls.Add(Me.FreqModLabel)
+        Me.Panel1.Controls.Add(Me.Frequency_Modifier)
+        Me.Panel1.Location = New System.Drawing.Point(1063, 37)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(189, 483)
+        Me.Panel1.TabIndex = 1
+        '
+        'SpeedLabel
+        '
+        Me.SpeedLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SpeedLabel.ForeColor = System.Drawing.Color.White
+        Me.SpeedLabel.Location = New System.Drawing.Point(3, 0)
+        Me.SpeedLabel.Name = "SpeedLabel"
+        Me.SpeedLabel.Size = New System.Drawing.Size(183, 23)
+        Me.SpeedLabel.TabIndex = 3
+        Me.SpeedLabel.Text = "Speed Modifier (100)"
+        Me.SpeedLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Speed_Modifier
+        '
+        Me.Speed_Modifier.Location = New System.Drawing.Point(3, 19)
+        Me.Speed_Modifier.Maximum = 400
+        Me.Speed_Modifier.Minimum = 1
+        Me.Speed_Modifier.Name = "Speed_Modifier"
+        Me.Speed_Modifier.Size = New System.Drawing.Size(183, 45)
+        Me.Speed_Modifier.TabIndex = 2
+        Me.Speed_Modifier.TickFrequency = 25
+        Me.Speed_Modifier.Value = 100
+        '
+        'FreqModLabel
+        '
+        Me.FreqModLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FreqModLabel.ForeColor = System.Drawing.Color.White
+        Me.FreqModLabel.Location = New System.Drawing.Point(3, 67)
+        Me.FreqModLabel.Name = "FreqModLabel"
+        Me.FreqModLabel.Size = New System.Drawing.Size(183, 23)
+        Me.FreqModLabel.TabIndex = 1
+        Me.FreqModLabel.Text = "Frequency Modifier (2)"
+        Me.FreqModLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Frequency_Modifier
+        '
+        Me.Frequency_Modifier.Location = New System.Drawing.Point(3, 86)
+        Me.Frequency_Modifier.Maximum = 5
+        Me.Frequency_Modifier.Minimum = 1
+        Me.Frequency_Modifier.Name = "Frequency_Modifier"
+        Me.Frequency_Modifier.Size = New System.Drawing.Size(183, 45)
+        Me.Frequency_Modifier.TabIndex = 0
+        Me.Frequency_Modifier.Value = 2
+        '
+        'PlayButton
+        '
+        Me.PlayButton.BackColor = System.Drawing.Color.RoyalBlue
+        Me.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.PlayButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PlayButton.ImageAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.PlayButton.Location = New System.Drawing.Point(485, 235)
+        Me.PlayButton.Name = "PlayButton"
+        Me.PlayButton.Size = New System.Drawing.Size(78, 84)
+        Me.PlayButton.TabIndex = 3
+        Me.PlayButton.Text = "▶"
+        Me.PlayButton.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        Me.PlayButton.UseVisualStyleBackColor = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuFile, Me.MenuView})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1264, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuFile
+        '
+        Me.MenuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem})
+        Me.MenuFile.ForeColor = System.Drawing.Color.White
+        Me.MenuFile.Name = "MenuFile"
+        Me.MenuFile.Size = New System.Drawing.Size(37, 20)
+        Me.MenuFile.Text = "File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'Output
+        '
+        Me.Output.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Output.Location = New System.Drawing.Point(12, 534)
+        Me.Output.Multiline = True
+        Me.Output.Name = "Output"
+        Me.Output.ReadOnly = True
+        Me.Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Output.Size = New System.Drawing.Size(1237, 132)
+        Me.Output.TabIndex = 4
+        '
+        'MenuView
+        '
+        Me.MenuView.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BackgroundSync, Me.TextColorSync})
+        Me.MenuView.ForeColor = System.Drawing.Color.White
+        Me.MenuView.Name = "MenuView"
+        Me.MenuView.Size = New System.Drawing.Size(44, 20)
+        Me.MenuView.Text = "View"
+        '
+        'BackgroundSync
+        '
+        Me.BackgroundSync.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.BackgroundSync.ForeColor = System.Drawing.Color.White
+        Me.BackgroundSync.Name = "BackgroundSync"
+        Me.BackgroundSync.Size = New System.Drawing.Size(227, 22)
+        Me.BackgroundSync.Text = "Background Color Sync (ON)"
+        '
+        'TextColorSync
+        '
+        Me.TextColorSync.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextColorSync.ForeColor = System.Drawing.Color.White
+        Me.TextColorSync.Name = "TextColorSync"
+        Me.TextColorSync.Size = New System.Drawing.Size(227, 22)
+        Me.TextColorSync.Text = "Text Color Sync (ON)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(1264, 682)
+        Me.Controls.Add(Me.PlayButton)
+        Me.Controls.Add(Me.Output)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MinimumSize = New System.Drawing.Size(1280, 720)
+        Me.Name = "Form1"
+        Me.Text = "Picture To Sound ~Dustin Harris"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.Speed_Modifier, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Frequency_Modifier, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Frequency_Modifier As TrackBar
+    Friend WithEvents SpeedLabel As Label
+    Friend WithEvents Speed_Modifier As TrackBar
+    Friend WithEvents FreqModLabel As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuFile As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PlayButton As Button
+    Friend WithEvents Output As TextBox
+    Friend WithEvents MenuView As ToolStripMenuItem
+    Friend WithEvents BackgroundSync As ToolStripMenuItem
+    Friend WithEvents TextColorSync As ToolStripMenuItem
+End Class
---
+++ PictureToSound/Form1.vb
@@ -0,0 +1,207 @@
+﻿Imports System.Threading
+Imports System.IO
+Imports System.Media
+Public Class Form1
+    'Private Declare Function Beep Lib "kernel32" (ByVal soundFrequency As Int32, ByVal soundDuration As Int32) As Int32
+    Public playthread As New Thread(AddressOf PlayPicture)
+    Public playing As Boolean = False
+    Public stopplaying As Boolean = False
+    Public AudioSync As New Object
+    Public py As Integer = 0
+    Public px As Integer = 0
+    Public filepath As String = ""
+    Dim lines() As String = IO.File.ReadAllLines("words.txt")
+    Dim backgroundsynctoggle As Boolean = True
+    Dim textsynctoggle As Boolean = True
+    Private Function GetColor(pic As PictureBox, X As Integer, Y As Integer) As Color
+
+        If pic Is Nothing Then Return Nothing
+
+        Using tmp As New Bitmap(pic.ClientSize.Width, pic.ClientSize.Height)
+
+            Dim r As New Rectangle(0, 0, tmp.Width, tmp.Height)
+
+            Using g As Graphics = Graphics.FromImage(tmp)
+                g.DrawImage(pic.Image, r, r, GraphicsUnit.Pixel)
+            End Using
+
+            Return tmp.GetPixel(X, Y)
+
+        End Using
+
+    End Function
+
+    Function PlayPicture()
+        Output.Clear()
+        If filepath IsNot "" Then
+            PictureBox1.Image = Image.FromFile(filepath)
+            stopplaying = False
+            playing = True
+            PlayButton.Visible = False
+            'Declare py,px as pixel positions
+            Dim pt As New Point(0, 0)
+            Dim g As Graphics
+            Dim red As Integer = 0
+            Dim green As Integer = 0
+            Dim blue As Integer = 0
+            Dim pixel As Color
+            Dim freq As Integer = 0
+            Dim textselect As Integer = 0
+            'Loop through each row of pixels in Picturebox1...
+            For py = 0 To PictureBox1.Image.Height - 1
+                For px = 0 To PictureBox1.Image.Width - 1
+                    pixel = GetColor(PictureBox1, px, py)
+                    red = pixel.R
+                    green = pixel.G
+                    blue = pixel.B
+                    freq = (red + green + blue) * Frequency_Modifier.Value
+                    textselect = red & green & blue
+                    While textselect > 354985
+                        If textselect > 354985 Then
+                            textselect = textselect / 3
+                        End If
+                    End While
+                    Output.Text = Output.Text & " " & lines(textselect)
+                    While freq > 4500 Or freq < 200
+                        If freq = 0 Then
+                            freq = 201
+                        ElseIf freq > 4500 Then
+                            freq = freq / 3
+                        ElseIf freq < 200 Then
+                            freq = freq * 3
+                        End If
+                    End While
+                    Beep(1000, freq, Speed_Modifier.Value)
+                    g = Graphics.FromImage(PictureBox1.Image)
+                    g.FillRectangle(Brushes.Green, px, py, 1, 1)
+                    PictureBox1.Invalidate()
+                    g.Dispose()
+                    If backgroundsynctoggle Then
+                        BackColor = pixel
+                    End If
+                    If textsynctoggle Then
+                        SpeedLabel.ForeColor = pixel
+                        FreqModLabel.ForeColor = pixel
+                        MenuFile.ForeColor = pixel
+                        MenuView.ForeColor = pixel
+                        BackgroundSync.ForeColor = pixel
+                        OpenToolStripMenuItem.ForeColor = pixel
+                        TextColorSync.ForeColor = pixel
+                    End If
+                    PlayButton.BackColor = pixel
+                    Application.DoEvents()
+                    If stopplaying = True Then
+                        Exit For
+                    End If
+                    While playing = False
+                        Application.DoEvents()
+                    End While
+                Next
+                If stopplaying = True Then
+                    Exit For
+                End If
+                While playing = False
+                    Application.DoEvents()
+                End While
+            Next
+            stopplaying = False
+        End If
+        Return 0
+    End Function
+    Private Sub Frequency_Modifier_Scroll(sender As Object, e As EventArgs) Handles Frequency_Modifier.Scroll
+        FreqModLabel.Text = "Frequency Modifier (" & Frequency_Modifier.Value & ")"
+    End Sub
+
+    Private Sub Speed_Modifier_Scroll(sender As Object, e As EventArgs) Handles Speed_Modifier.Scroll
+        SpeedLabel.Text = "Speed Modifier (" & Speed_Modifier.Value & ")"
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        stopplaying = True
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+        Dim strFileName As String
+        fd.Title = "Choose Image To Listen Too"
+        fd.Filter = "All files (*.*)|*.*"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+        PictureBox1.ImageLocation = filepath
+        playing = False
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+            filepath = fd.FileName
+            PictureBox1.Image = Image.FromFile(filepath)
+            PlayPicture()
+        End If
+    End Sub
+
+    Private Sub PlayButton_Click(sender As Object, e As EventArgs) Handles PlayButton.Click
+        If playing = False Then
+            playing = True
+            PlayPicture()
+        End If
+    End Sub
+    Sub Beep(ByVal Amplitude As Integer,
+             ByVal Frequency As Integer,
+             ByVal Duration As Integer)
+        Dim A As Double = ((Amplitude * 2 ^ 15) / 1000) - 1
+        Dim DeltaFT As Double = 2 * Math.PI * Frequency / 44100
+
+        Dim Samples As Integer = 441 * Duration \ 10
+        Dim Bytes As Integer = Samples * 4
+        Dim Hdr() As Integer = {&H46464952, 36 + Bytes, &H45564157,
+                                &H20746D66, 16, &H20001, 44100,
+                                 176400, &H100004, &H61746164, Bytes}
+        Using MS As New MemoryStream(44 + Bytes)
+            Using BW As New BinaryWriter(MS)
+                For I As Integer = 0 To Hdr.Length - 1
+                    BW.Write(Hdr(I))
+                Next
+                For T As Integer = 0 To Samples - 1
+                    Dim Sample As Short = CShort(A * Math.Sin(DeltaFT * T))
+                    BW.Write(Sample)
+                    BW.Write(Sample)
+                Next
+                BW.Flush()
+                MS.Seek(0, SeekOrigin.Begin)
+                Using SP As New SoundPlayer(MS)
+                    SP.PlaySync()
+                End Using
+            End Using
+        End Using
+    End Sub
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        PlayButton.Text = "▶"
+        PlayButton.Visible = True
+        playing = False
+        stopplaying = True
+    End Sub
+
+    Private Sub BackgroundSync_Click(sender As Object, e As EventArgs) Handles BackgroundSync.Click
+        If backgroundsynctoggle Then
+            BackgroundSync.Text = "Background Color Sync (OFF)"
+            backgroundsynctoggle = False
+            BackColor = SystemColors.ControlDarkDark
+        Else
+            BackgroundSync.Text = "Background Color Sync (ON)"
+            backgroundsynctoggle = True
+        End If
+    End Sub
+
+    Private Sub TextColorSync_Click(sender As Object, e As EventArgs) Handles TextColorSync.Click
+        If textsynctoggle Then
+            SpeedLabel.ForeColor = Color.White
+            FreqModLabel.ForeColor = Color.White
+            MenuFile.ForeColor = Color.White
+            MenuView.ForeColor = Color.White
+            BackgroundSync.ForeColor = Color.White
+            OpenToolStripMenuItem.ForeColor = Color.White
+            TextColorSync.ForeColor = Color.White
+            TextColorSync.Text = "Text Color Sync (OFF)"
+            textsynctoggle = False
+            BackColor = SystemColors.ControlDarkDark
+        Else
+            TextColorSync.Text = "Text Color Sync (ON)"
+            textsynctoggle = True
+        End If
+    End Sub
+End Class
